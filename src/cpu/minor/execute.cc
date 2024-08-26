bac750748817b682e13c02e3bcdec7dfb3d2845e
poal023 <91771571+poal023@users.noreply.github.com>
2024-08-25 23:50:18 -0500

    cpu-minor: Added commit stats to commitInstAccounting
    Change-Id: I5467b286688490e9658918b0d3bb3ba0cde1deea

--- src/cpu/minor/execute.cc
+++ src/cpu/minor/execute.cc
@@ -884,7 +884,28 @@ Execute::doInstCommitAccounting(MinorDynInstPtr inst)
     {
         thread->numInst++;
         thread->threadStats.numInsts++;
-        cpu.commitStats[tid]->numInsts++;
+        cpu.commitStats[inst->id.threadId]->numInsts++;
+
+        if (inst->staticInst->isInteger()) {
+            cpu.commitStats[inst->id.threadId]->numIntInsts++;
+        }
+
+        if (inst->staticInst->isFloating()) {
+            cpu.commitStats[inst->id.threadId]->numFpInsts++;
+        }
+
+        if (inst->staticInst->isMemRef()) {
+            cpu.commitStats[inst->id.threadId]->numMemRefs++;
+        }
+
+        if (inst->staticInst->isLoad()) {
+            cpu.commitStats[inst->id.threadId]->numLoadInsts++;
+        }
+
+        if (inst->staticInst->isStore() || inst->staticInst->isAtomic()) {
+            cpu.commitStats[inst->id.threadId]->numStoreInsts++;
+        }
+
         cpu.baseStats.numInsts++;
         if (in_user_mode) {
             cpu.commitStats[tid]->numUserInsts++;
