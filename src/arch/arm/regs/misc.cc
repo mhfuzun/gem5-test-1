c3b03319d846b959054aaa030c77555059a16685
Giacomo Travaglini <giacomo.travaglini.arm.com>
2025-09-12 12:45:24 +0100

    arch-arm: Allow non-serializing updates of TPIDR_ELx register
    The TPIDR_ELx registers are scratch registers software
    can read/write to.
    
    As the PE makes no use of those registers, they
    can be read/written speculatively to improve performance
    without serializing execution
    
    Signed-off-by: Giacomo Travaglini <giacomo.travaglini.arm.com>
    Change-Id: Ifc5f22f253ff4e020859d13982aaafa971935832

--- src/arch/arm/regs/misc.cc
+++ src/arch/arm/regs/misc.cc
@@ -3140,6 +3140,7 @@ ISA::initializeMiscRegMetadata()
      *  architecturally mandated."
      */
 
+    // clang-format off
     InitReg(MISCREG_CPSR)
       .reset(resetCPSR(system))
       .allPrivileges();
@@ -6036,11 +6037,13 @@ ISA::initializeMiscRegMetadata()
       .mapsTo(MISCREG_CONTEXTIDR_NS);
     InitReg(MISCREG_TPIDR_EL1)
       .allPrivileges().exceptUserMode()
+      .serializing(false)
       .faultRead(EL1, faultFgtEL1<true, &HFGTR::tpidrEL1>)
       .faultWrite(EL1, faultFgtEL1<false, &HFGTR::tpidrEL1>)
       .mapsTo(MISCREG_TPIDRPRW_NS);
     InitReg(MISCREG_TPIDR_EL0)
       .allPrivileges()
+      .serializing(false)
       .faultRead(EL0, faultFgtEL0<true, &HFGTR::tpidrEL0>)
       .faultWrite(EL0, faultFgtEL0<false, &HFGTR::tpidrEL0>)
       .faultRead(EL1, faultFgtEL1<true, &HFGTR::tpidrEL0>)
@@ -6054,9 +6057,11 @@ ISA::initializeMiscRegMetadata()
       .mapsTo(MISCREG_TPIDRURO_NS);
     InitReg(MISCREG_TPIDR_EL2)
       .hyp().mon()
+      .serializing(false)
       .mapsTo(MISCREG_HTPIDR);
     InitReg(MISCREG_TPIDR_EL3)
-      .mon();
+      .mon()
+      .serializing(false);
     // BEGIN Generic Timer (AArch64)
     InitReg(MISCREG_CNTFRQ_EL0)
       .reads(1)
@@ -7074,7 +7079,7 @@ ISA::initializeMiscRegMetadata()
         .mon();
     InitReg(MISCREG_PIR_EL3)
         .mon();
-
+    // clang-format on
 
     // Register mappings for some unimplemented registers:
     // ESR_EL1 -> DFSR
